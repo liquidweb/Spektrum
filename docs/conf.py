d39042d3066b995b80afadd16c9fbc49b1cd7700
Desmond McDermitt <dmcdermitt@liquidweb.com>
2022-09-13 13:17:24 -0400

    MQ-430: added Liquidweb to license

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Spektrum'
-copyright = u'2013-2017, John Vrbanac'
+copyright = u'2021, LiquidWeb'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
