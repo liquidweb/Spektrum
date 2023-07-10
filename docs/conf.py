bd2d14e8616d3cd254612218085a5b7f67ab3425
Desmond McDermitt <dmcdermitt@liquidweb.com>
2023-07-10 13:20:04 -0400

    Bump version: 1.0.1 → 1.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -31,8 +31,8 @@ copyright = u'2021, LiquidWeb'
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 
-version = '1.0.1'
-release = '1.0.1'
+version = '1.1.0'
+release = '1.1.0'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
